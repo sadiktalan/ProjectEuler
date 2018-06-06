4e8f5cec18231f4bd2bd4776d0e74f9a374b10ac
SadikTal <talansadik@gmail.com>
2018-06-06 17:38:19 +0300

    Problem 1 Solved

---
+++ ProjectEulerProblems/ProjectEulerProblems/Problem1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEulerProblems
+{
+    class Problem1
+    {
+        public static int MultipliesOf3And5(int number)
+        {
+            var total = 0;
+            var counter = 0;
+            while (counter < number)
+            {
+                if (counter % 15 == 0)
+                {
+                    total += counter;
+                }
+                else if (counter % 5 == 0)
+                {
+                    total += counter;
+                }
+                else if (counter % 3 == 0)
+                {
+                    total += counter;
+                }
+
+                counter++;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ProjectEulerProblems/ProjectEulerProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectEulerProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Problem1.MultipliesOf3And5(1000));
+            Console.ReadLine();
+        }
+    }
+}
